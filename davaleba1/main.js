697ae3c670e2e03a7a12491461fff9516a0b248b
Ana Mtselishvili <anamtselishvili@Anas-MacBook-Pro.local>
2023-03-30 16:56:31 +0200

    add davaleba 2

--- davaleba1/main.js
+++ davaleba1/main.js
@@ -1,15 +1,27 @@
-// დაწერეთ ფუნქცია calculateUniversalArea რომელიც დააბრუნებს მასივის (areaSidesList) ყველა ელემენტის ჯამს
-let areaSidesList = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20];
 
-function calculateUniversalArea(list) {
-    let result = 0;
-    for (let x = 0; x < list.length; x++) {
-       result = result + list[x];
-    }
-    return result;
-}
 
-console.log(
-    calculateUniversalArea(areaSidesList)//130
-);
+const names = ['levani', 'ana', 'irakli', 'beso'];
+const box = ['koba', 'giorgi', 'masho',];
+const numbers = [4, 7, 3, 8, 9, 2, 4];
+const container= [3, 4, 6, 2,[3 ,4, 4 ,[4] ], 3];
 
+const numbers1 = numbers.sort()
+const numbers2 = numbers.reduce((total,currentValue) => total + currentValue);
+const numbers3 = numbers.some(n => 6);
+const numbers4 = numbers.every((n)=> n > 3);
+const combined = names.concat(box);
+const names2 = names.push("alex");
+const names3 = names.slice(1, 3);
+const container1 = container.flat(2);
+
+
+
+
+console.log(numbers1);
+console.log(numbers2);
+console.log(numbers3);
+console.log(numbers4);
+console.log(combined);
+console.log(names2);
+console.log(names3);
+console.log(container1);
